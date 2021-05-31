6f4ee0ff965a32b2f7e0150a21aa3e520c691e96
Marlon Sousa <franciscomarlonsousa8@gmail.com>
2021-05-31 11:27:49 -0300

    Install Deleted

--- install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install python3
-sudo apt install python3-pyqt5
-sudo apt install python3-pip
-sudo python3 -m pip install matplotlib
-sudo python3 -m pip install sympy
-sudo python3 -m pip install ipyth sympy
-sudo python3 -m pip install ipython
-sudo python3 -m pip install numpy
